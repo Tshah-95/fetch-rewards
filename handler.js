ddd04c5204ee056be0b58ccc2a071cd7d832393a
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2022-05-31 23:28:43 -0400

    allow overwriting of historical transaction

--- handler.js
+++ handler.js
@@ -25,7 +25,7 @@ exports.createTransaction = async (event) => {
         // need to add check for time being invalid (poorly formatted or in future)
 
         if (points > 0) {
-            await db.put({ TableName: Transactions, Item: { partner, points, time }, Expected: { Exists: false } }).promise()
+            await db.put({ TableName: Transactions, Item: { partner, points, time } }).promise()
             return buildResponse(200, { partner, points, time }, "Transaction successfully posted!")
         } else {
             // retreive all transactions for the current partner
@@ -44,7 +44,7 @@ exports.createTransaction = async (event) => {
             // if the point total is more negative than the balance of the partner, prevent the action
             if (partnerPointTotal + points < 0) return buildResponse(422, event, "Action results in negative balance")
 
-            await db.put({ TableName: Transactions, Item: { partner, points, time }, Expected: { Exists: false } }).promise()
+            await db.put({ TableName: Transactions, Item: { partner, points, time } }).promise()
 
             return buildResponse(200, { partner, points, time }, "Transaction successfully posted!")
         }
