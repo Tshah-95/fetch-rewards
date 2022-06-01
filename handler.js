056333b9c2b8ca7cfb4643a6620fc8793bf1084e
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2022-06-01 12:37:19 -0400

    balance endpoint complete
    send parsed transaction instead of event on 4xx events
    Aggregate balances in balance endpoint and return json with partner as key

--- handler.js
+++ handler.js
@@ -16,13 +16,14 @@ exports.createTransaction = async (event) => {
 
         // check for all necessary parameters of a transaction
         if (!(parsedTransaction?.partner && parsedTransaction?.points && parsedTransaction?.time)) {
-            return buildResponse(422, event, "Improper Body Parameters Provided")
+            return buildResponse(422, parsedTransaction, "Improper Body Parameters Provided")
         }
 
         // destructure properties since they are confirmed as defined
         const { partner, points, time } = parsedTransaction
 
         // need to add check for time being invalid (poorly formatted or in future)
+        // if transaction with pre-existing partner and time comes through, there will be unintentded functionality
 
         if (points > 0) {
             await db.put({ TableName: Transactions, Item: { partner, points, time } }).promise()
@@ -36,13 +37,13 @@ exports.createTransaction = async (event) => {
             }).promise()
 
             // create an accumulator that starts at 0 and adds the points of each transaction.
-            // this effectively returns the current point count for a given partner
+            // this effectively returns the active balance for a given partner
             let partnerPointTotal = partnerTransactions.Items.reduce(
                 (prevVal, currItem) => prevVal + currItem.points, 0
             )
 
             // if the point total is more negative than the balance of the partner, prevent the action
-            if (partnerPointTotal + points < 0) return buildResponse(422, event, "Action results in negative balance")
+            if (partnerPointTotal + points < 0) return buildResponse(422, parsedTransaction, "Action results in negative balance")
 
             await db.put({ TableName: Transactions, Item: { partner, points, time } }).promise()
 
@@ -62,10 +63,25 @@ exports.spendPoints = async (event) => {
     }
 }
 
+// This endpoint simply returns the sums of each transaction by-partner, aka their active balance
 exports.getBalances = async (event) => {
     try {
-        const result = await db.scan({ TableName: Transactions }).promise()
-        return buildResponse(200, result.Items, "Result returns current transactions")
+        const allTransactions = await db.scan({ TableName: Transactions }).promise()
+
+        const balanceMap = new Map()
+
+        for (const transaction of allTransactions.Items) {
+            const { partner, points } = transaction
+
+            // if map already contains partner, combine values to result in total by-partner
+            // else simply insert current transaction as baseline
+            if (balanceMap.has(partner)) {
+                let oldVal = balanceMap.get(partner)
+                balanceMap.set(partner, oldVal + points)
+            } else balanceMap.set(partner, points)
+        }
+
+        return buildResponse(200, Object.fromEntries(balanceMap), "Successfully Retreived Active Balances!")
     } catch (e) {
         return buildResponse(500, event, e.message)
     }
